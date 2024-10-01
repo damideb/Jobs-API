a69e551a494bcfe24f700766a9b2c6bcc470351d
Damilola Adebowale <adebzdamzy@gmail.com>
2024-10-01 14:52:47 +0100

    bug fix

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
+    app.listen(PORT, () =>
       console.log(`Server is listening on port ${PORT}...`)
     );
   } catch (error) {
